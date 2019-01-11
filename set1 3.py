c095a519c554e572de601b72a8ee41df50fbba18
perezoso001 <46412523+perezoso001@users.noreply.github.com>
2019-01-11 11:18:07 +0530

    Delete set1 3.py

--- set1 3.py
+++
@@ -1,7 +0,0 @@
-i=raw_input()
-if i=='a' or i=='A' or i=='e' or i=='E' or i=='i' or i=='I' or i=='o' or i=='O' or i=='u' or i=='U':
-    print "Vowel"
-elif i.isalpha():
-    print "consonant"
-else:
-    print"invalid"
